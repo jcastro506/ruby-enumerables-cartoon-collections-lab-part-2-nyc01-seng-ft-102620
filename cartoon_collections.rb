aeb702b260daa95a02fd66128a03333846ec32de
Josh Castro <castrojoshua805@gmail.com>
2020-10-12 00:05:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,25 +1,37 @@
+def roll_call_dwarves# code an argument here
+  # Your code here
+end 
+
 def square_array(array)
-  array.map do |element| 
-    element * element 
-  end 
+  # Use an Enumerable to square every element in the passed in array
+  # Return a new array of the results
 end
+
+def summon_captain_planet# code an argument here
+  # Your code here
+end 
 
 def summon_captain_planet(planeteer_calls)
-    planeteer_calls.map do |words|
-      "#{words.capitalize}!"
-    end 
+  # Use an Enumerable to capitalize and add '!' to every element in the passed in array
+  # Return a new array of the results
 end
 
+def long_planeteer_calls# code an argument here
+  # Your code here
+end 
+
 def long_planeteer_calls(planeteer_calls)
-  planeteer_calls.any? do |words|
-    words.length > 4 
-  end 
+  # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
+  # Return the boolean result of this check
 end
+
+def find_the_cheese# code an argument here
+  # the array below is here to help
+  cheese_types = ["cheddar", "gouda", "camembert"]
+end 
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find do |words|
-    valid_calls.include?(words)
-  end 
+  # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above 
+  # Return the first valid call found, or return nil if no valid calls are found
 end
-
