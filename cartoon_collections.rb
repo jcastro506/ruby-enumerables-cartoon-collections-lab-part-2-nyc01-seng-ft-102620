3d7dddce1832d1c5af6ded977966b201e23ebe70
Josh Castro <castrojoshua805@gmail.com>
2020-10-12 00:50:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,7 @@ def summon_captain_planet
 end 
 
 def summon_captain_planet(planeteer_calls)
-  # Use an Enumerable to capitalize and add '!' to every element in the passed in array
-  # Return a new array of the results
+  
 end
 
 def long_planeteer_calls# code an argument here
