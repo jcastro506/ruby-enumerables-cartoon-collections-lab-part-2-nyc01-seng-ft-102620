cfbb7744631647dd69cf8520e40fa806d18f7978
Josh Castro <castrojoshua805@gmail.com>
2020-10-12 01:11:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,8 +22,9 @@ def long_planeteer_calls# code an argument here
 end 
 
 def long_planeteer_calls(planeteer_calls)
-  # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
-  # Return the boolean result of this check
+  planeteer_calls.any? do |words|
+    words.length > 3 
+  end 
 end
 
 def find_the_cheese# code an argument here
