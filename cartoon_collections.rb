fc2b9985767947e0e2178ca0c5259441d6ac557b
Josh Castro <castrojoshua805@gmail.com>
2020-10-12 01:24:20 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -35,5 +35,6 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |words|
-    
+    valid_calls.include?(words)
+  end 
 end
